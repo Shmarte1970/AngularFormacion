3027896256279e437dfb7e0c1e84c4bd81c58831
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-10-11 15:00:59 +0200

    nuevo update de proyecto

--- empleadoList/src/app/components/empleado-list/empleado-list.component.ts
+++ empleadoList/src/app/components/empleado-list/empleado-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Empleado } from 'src/app/models/empleados';
 
 @Component({
   selector: 'app-empleado-list',
   templateUrl: './empleado-list.component.html',
   styleUrls: ['./empleado-list.component.css']
 })
-export class EmpleadoListComponent {
+export class EmpleadoListComponent implements OnInit {
+
+  listEmpleados: Empleado[] = [
+    {legajo:1, nombre: 'Estrella', apellido:'Paris', sexo: 'Femenino', salario:18000},
+    {legajo:2, nombre: 'Barbara', apellido:'Fruitos', sexo: 'Femenino', salario:18000},
+    {legajo:3, nombre: 'Armando', apellido:'Bronca', sexo: 'Masculino', salario:18000},
+    {legajo:4, nombre: 'Fernando', apellido:'Trunollosa', sexo: 'Masculino', salario:18000}
+  ];
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void{
+
+  }
 
 }
