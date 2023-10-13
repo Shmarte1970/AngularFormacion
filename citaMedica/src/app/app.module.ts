cc7acaabaf986e3692c52c33e3467e20c7191681
Shmarte1970 <shmarte@gmail.com>
2023-10-13 15:17:33 +0200

    citas medicas

---
+++ citaMedica/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ListCitasComponent } from './components/list-citas/list-citas.component';
+import { CrearCitaComponent } from './components/crear-cita/crear-cita.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListCitasComponent,
+    CrearCitaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
