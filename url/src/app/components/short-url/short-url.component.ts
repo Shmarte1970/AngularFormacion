48058bbe6cdff82fbd8b5eae8f5d2cb0028bdc5e
Shmarte1970 <shmarte@gmail.com>
2023-11-24 14:46:42 +0100

    update poryecto acortar url

--- url/src/app/components/short-url/short-url.component.ts
+++ url/src/app/components/short-url/short-url.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShortUrlService } from 'src/app/services/short-url.service';
 
 @Component({
   selector: 'app-short-url',
   templateUrl: './short-url.component.html',
   styleUrls: ['./short-url.component.css']
 })
-export class ShortUrlComponent {
+export class ShortUrlComponent implements OnInit{
+
+  nombreUrl = '';
+  urlShort = '';
+  urlProcesada: boolean;
+  
+
+  constructor(private _shortUrlService:ShortUrlService){
+
+    this.nombreUrl
+    this.urlShort
+    this.urlProcesada = false
+
+
+
+  }
+
+  
+
+  ngOnInit(): void {
+    
+  }
+
+  procesarURL(){
+      this._shortUrlService.getUrlShort(this.nombreUrl).subscribe(data => {
+        console.log(data);
+      })
+    
+  }
 
 }
--- url/src/app/services/short-url.service.ts
+++ url/src/app/services/short-url.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShortUrlService {
 
-  constructor() { }
+  url = 'https://api-ssl.bitly.com/v4/shorten';
+  token = '3afd96810f79a56da63980e5415065af97693b9b';
+
+  constructor(private http: HttpClient) { }
+
+  getUrlShort(nombreUrl: string): Observable<any>{
+
+    const tokenHeader = new HttpHeaders({authorization: `Bearer` + this.token})
+
+    const body = {
+      long_url: nombreUrl
+    }
+    return this.http.post(this.url, body, {headers:tokenHeader});
+  }
 }
