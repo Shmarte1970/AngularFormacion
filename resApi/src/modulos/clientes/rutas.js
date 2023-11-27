9a58f33291b203b8052c60565fdc2d9f3d5ae02f
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-11-27 14:58:12 +0100

    update resApi

--- resApi/src/modulos/clientes/rutas.js
+++ resApi/src/modulos/clientes/rutas.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
+const respuesta = require("../../red/respuestas");
+
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  res.send("Pagina Clientes OK");
+  respuesta.success(req, res, "Todo Ok", 200);
 });
 
 module.exports = router;
--- resApi/src/red/respuestas.js
+++ resApi/src/red/respuestas.js
@@ -1,7 +1,21 @@
-export.success = function (req, res, mensajes, status){
-    res.status(status).send({
-        error: false,
-        status: status,
-        body: mensaje
-    });
-}
+exports.success = function (req, res, mensajes, status) {
+  const statusCode = status || 200;
+  const mensajeOk = mensaje || "";
+
+  res.status(statusCode).send({
+    error: false,
+    status: statusCode,
+    body: mensajeOk,
+  });
+};
+
+exports.error = function (req, res, mensajes, status) {
+  const statusCode = status || 500;
+  const mensajeError = mensaje || "Error Interno";
+
+  res.status(statusCode).send({
+    error: true,
+    status: statusCode,
+    body: mensajeError,
+  });
+};
